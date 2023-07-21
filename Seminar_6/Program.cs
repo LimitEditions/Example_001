30a0b0616ae3d30811c7f97b8f3e67eef1107479
Anastasiya <007-badgerl@rambler.ru>
2023-07-21 16:59:35 +0300

    добавили решение задач к семинару 6

---
+++ Seminar_6/Program.cs
@@ -0,0 +1,57 @@
+﻿
+// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// int num = 5;
+// int digit;
+// int count = 0;
+// int i = 0;
+
+// while (i < num){
+//     Console.WriteLine("Введите число");
+//     digit = Convert.ToInt32(Console.ReadLine());
+//         if(digit > 0){
+//         count++;
+       
+//     }
+//     i++;
+// }
+// Console.WriteLine("Чисел больше 0: " + count);
+
+// int digit = 1;
+// int count = 0;
+
+// while (digit != 0){
+//     Console.WriteLine("Вводите числа, пока не нажмете 0");
+//     digit = Convert.ToInt32(Console.ReadLine());
+//         if(digit > 0){
+//         count++;
+//     }
+// }
+// Console.WriteLine("Чисел больше 0: " + count);
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// System.Console.WriteLine("Введите число b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = 0, y = 0;
+// x = (b2 - b1)/(k1 - k2);
+// y = k1 * x + b1;
+
+// System.Console.WriteLine($"Точка пересечения имеет координаты х = {x} и у = {y}");
+
+
+
